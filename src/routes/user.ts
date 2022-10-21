983897f9ca71628d5e3ce11fad165fe9a9e3a23f
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-10-21 10:50:02 +0300

    removed unused imports

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,4 @@
-import express, { Router, Request, Response } from 'express'
+import express, { Router } from 'express'
 
 import authMiddleware from '../middlewares/auth'
 import getUserMiddleware from '../middlewares/getUser'
