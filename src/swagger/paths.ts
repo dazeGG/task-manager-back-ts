19ba9554240e18f478a09d21c6242f5c49b77c41
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-10-26 22:20:54 +0300

    some swagger fixes

--- src/swagger/paths.ts
+++ src/swagger/paths.ts
@@ -1,21 +1,51 @@
 export default {
-    '/user/sign-up': {
-      post: {
-        tags: ['User'],
-        summary: 'Endpoint for registration user',
-        parameters: {
-            username: {
-                
-            }
+  '/user/sign-up': {
+    post: {
+      tags: ['User'],
+      summary: 'Endpoint for registration user',
+      consumes: ['application/json'],
+      parameters: [
+        {
+          in: 'body',
+          name: 'User',
+          description: 'The user`s data to create',
+          schema: {
+            $ref: '#/definitions/User'
+          }
+        }
+      ],
+      responses: {
+        201: {
+          description: 'Created',
+          content: '{ token }'
+        }
+      }
+    }
+  },
+  '/user/sign-in': {
+    post: {
+      tags: ['User'],
+      summary: 'Endpoint for authorization user',
+      parameters: [
+        {
+          name: 'username',
+          in: 'body',
+          type: 'string'
         },
-        responses: {
-          200: {
-            description: 'OK',
-            schema: {
-              '$ref': '#/definitions/User'
-            }
+        {
+          name: 'password',
+          in: 'body',
+          type: 'string'
+        }
+      ],
+      responses: {
+        200: {
+          description: 'OK',
+          schema: {
+            $ref: '#/definitions/User'
           }
         }
       }
     }
   }
+}
