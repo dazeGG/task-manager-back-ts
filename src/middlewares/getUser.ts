efb4c0c09d0df4efa7e44e65741a40e604013320
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-10-26 22:21:44 +0300

    make send token through authorization header

--- src/middlewares/getUser.ts
+++ src/middlewares/getUser.ts
@@ -2,11 +2,17 @@ import { Request, Response, NextFunction } from 'express'
 import Users from '../models/User'
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-    const user = await Users.findOne({ token: req.headers.bearer })
-    if (user) {
-        res.locals.user = user
-        next()
+    const authorizationItems = (req.headers.authorization || '').split(' ')
+    if (authorizationItems[0] === 'Brearer') {
+        const token = authorizationItems[1]
+        const user = await Users.findOne({ token })
+        if (user) {
+            res.locals.user = user
+            next()
+        } else {
+            res.status(401).send('User with this token not found')
+        }
     } else {
-        res.status(401).send('User with this token not found')
+        res.status(400).send('Make sure you have sent the token correctly')
     }
 }
