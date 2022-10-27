b8710d2a318f6dc614772e0df175081294bc2112
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-10-27 03:00:08 +0300

    code refactoring & added endpoint to delete group

--- src/controllers/group.ts
+++ src/controllers/group.ts
@@ -9,7 +9,7 @@ import Tasks from '../models/Task'
 import getGroupFull from '../scripts/getGroupFull'
 
 class groupController {
-    async getGroup(req: Request, res: Response) {
+    async get(req: Request, res: Response) {
         const groupID = req.params.id
         if (res.locals.user.groups.includes(groupID)) {
             res.status(200).send(await getGroupFull(groupID))
@@ -27,14 +27,14 @@ class groupController {
     //     await user.save()
     //     res.status(201).send(await getGroupFull(group._id))
     // }
-    async createGroup(req: Request, res: Response) {
+    async create(req: Request, res: Response) {
         const group = await Groups.create({ title: req.body.title })
         const user = res.locals.user
         user.groups.push(group._id)
         await user.save()
         res.status(201).send(await getGroupFull(group._id))
     }
-    async updateGroup(req: Request, res: Response) {
+    async update(req: Request, res: Response) {
         const group = await Groups.findById(req.body._id)
         if (group) {
             group.title = req.body.title
@@ -43,6 +43,11 @@ class groupController {
         } else {
             res.status(404).send('Group with this id was not found')
         }
+    }
+    async delete(req: Request, res: Response) {
+        const group = await Groups.findByIdAndDelete(req.body._id)
+        if (group) res.sendStatus(204)
+        else res.status(404).send('Invalid group id')
     }
 }
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,24 @@ import express, { Router } from 'express'
 
 // Routers
 import userRouter from './user'
-import groupRouter from './group'
+
+// Middlewares
+import getUserMiddleware from '../middlewares/getUser'
+
+// Controllers
+import groupController from '../controllers/group'
 
 const router: Router = express.Router()
 
+// User
 router.use('/user', userRouter)
-router.use('/group', groupRouter)
+
+// Group
+router.use('/group', getUserMiddleware)
+
+router.get('/group/:id', groupController.get)
+router.post('/group', groupController.create)
+router.put('/group', groupController.update)
+router.delete('/group', groupController.delete)
 
 export default router
