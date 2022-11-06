d46a7d7b86afe34a4edd24c00a1f2cd4dae35516
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-11-06 13:36:37 +0300

    minify comments

--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,15 +1,11 @@
 // Express and Router type
 import express, { Router } from 'express'
 
-// Middlewares
 import getUserMiddleware from '../middlewares/getUser'
-
-// Controllers
 import groupController from '../controllers/group'
 
 const router: Router = express.Router()
 
-// Group
 router.use('/', getUserMiddleware)
 
 router.post('/', groupController.create)
