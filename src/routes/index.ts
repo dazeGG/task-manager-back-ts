4ac4a07380b93d853068091895779e220dd9fc31
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-11-06 13:43:13 +0300

    /auth: index -> user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,11 @@
 import express, { Router } from 'express'
 
 // Routers
-import authRouter from './auth'
 import userRouter from './user'
 import groupRouter from './group'
 
 const router: Router = express.Router()
 
-router.use('/auth', authRouter)
 router.use('/user', userRouter)
 router.use('/group', groupRouter)
 
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,8 +3,11 @@ import express, { Router } from 'express'
 
 import getUserMiddleware from '../middlewares/getUser'
 import userController from '../controllers/user'
+import authRouter from './auth'
 
 const router: Router = express.Router()
+
+router.use('/auth', authRouter)
 
 // * Get Groups
 
