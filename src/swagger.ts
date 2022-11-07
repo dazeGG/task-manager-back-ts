10c03a161ecf47883b58213733076abff64a2802
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-11-08 02:40:13 +0300

    some swagger things

--- src/swagger.ts
+++ src/swagger.ts
@@ -25,6 +25,7 @@ export default {
     tags,
     components,
     paths: {
+        ...YAML.load(path.join(__dirname, '..', 'swagger', 'paths', 'task.yaml')),
         ...YAML.load(path.join(__dirname, '..', 'swagger', 'paths', 'user.yaml')),
         ...YAML.load(path.join(__dirname, '..', 'swagger', 'paths', 'group.yaml'))
     }
